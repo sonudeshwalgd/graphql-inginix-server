1bad4fc6fa850d5913a826a086435bfe5747ab8e
sonudeshwalgd <101305714+sonudeshwalgd@users.noreply.github.com>
2023-07-22 15:58:22 +0530

    updated product routes

--- routes/v1/homepageRoutes/index.js
+++ routes/v1/homepageRoutes/index.js
@@ -2,7 +2,7 @@ const express=require("express")
 const { dashbardController } = require("../../../controller")
 const homePageRouter=express.Router()
 
-homePageRouter.post("/product",dashbardController.postProduct)
-homePageRouter.get("/product",dashbardController.getProduct)
+homePageRouter.post("/add-product",dashbardController.postProduct)
+homePageRouter.post("/get-product",dashbardController.getProduct)
 
 module.exports=homePageRouter
